1d68754300f8ea7d9903371675d4abb70448020e
Ciprian Marin <ciprimarin2001@gmail.com>
2023-03-30 16:30:36 +0300

    added missing doc files

---
+++ source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NapoleonAI'
+copyright = '2023, Ciprian Marin'
+author = 'Ciprian Marin'
+release = '0.0.7'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+autodoc_default_options = {
+    'members': True,
+}
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../napoleonai'))
+
---
+++ source/index.rst
@@ -0,0 +1,24 @@
+.. NapoleonAI documentation master file, created by
+   sphinx-quickstart on Mon Mar 27 12:12:08 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to NapoleonAI's documentation!
+======================================
+
+.. toctree::
+   :caption: Contents:
+
+   usage
+   demo
+   configuration
+   documentation
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
