b04f9a34088f925de89f922379c1ac98d49fd02e
Ciprian Marin <ciprimarin2001@gmail.com>
2023-06-11 22:29:58 +0300

    added resnet

--- src/napoleonai/imageclassification/image_models_dictionary.py
+++ src/napoleonai/imageclassification/image_models_dictionary.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 CONF = {
-    'EfficientNetV2B3': (tf.keras.applications.efficientnet_v2.EfficientNetV2B3, tf.keras.applications.efficientnet_v2.preprocess_input)
+    'EfficientNetV2B3': (tf.keras.applications.efficientnet_v2.EfficientNetV2B3, tf.keras.applications.efficientnet_v2.preprocess_input),
+    'Resnet50': (tf.keras.applications.resnet50.ResNet50, tf.keras.applications.resnet50.preprocess_input)
 }
 
